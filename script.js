f30adc28841395410d89c024d478672ab90f289b
Nix7amcm <65091582+Nix7amcm@users.noreply.github.com>
2023-05-19 19:23:32 +0100

    Update styles and script
    - root.css variables syntax fix
    - page styled
    - add animation and sound effect (code annotated)

--- script.js
+++ script.js
@@ -9,10 +9,37 @@ function handleSubmit ( event ) {
   const rating = new FormData( event.target ).get( "rating" );
 
   if ( rating ) {
-    wrapper.innerHTML = template.innerHTML.replace( /{{ rating }}/, rating );
-    //::::: when rating is selected and submitted, 'wrapper' (main) will be replaced with 'template', and selected rating will replace {{ rating }} string.
+
+    wrapper.addEventListener( "animationend", () => {
+      //::::: wait until animation (css keyframes) has ended to trigger replace rating below
+
+      wrapper.innerHTML = template.innerHTML.replace( /{{ rating }}/, rating );
+      //::::: when rating is selected and submitted, 'wrapper' will be replaced with 'template', and selected rating will replace {{ rating }} html string
+
+      wrapper.classList.replace( "animate-out", "animate-in" );
+      //::::: replace class animate-out with animate-in (trigger animations in correct order)
+    },
+      {
+        once: true,
+      } );
+
+    playSound();
+
+    wrapper.classList.add( "animate-out" );
+    //::::: add new class to wrapper to apply animation
+
+    wrapper.style.height = wrapper.offsetHeight + "px";
+    //::::: take height of original main wrapper and apply it to template as inline style
+
   }
 }
 
+function playSound () {
+  const audio = new Audio( "sounds/multimedia_max_min_interface_sound_whoosh_max.mp3" );
+
+  audio.volume = 0.2;
+  audio.play();
+}
+
 form.addEventListener( "submit", handleSubmit );
-//::::: 'Submit' button will trigger 'handleSubmit' function
+//::::: trigger handleSubmit' function upon 'Submit' button event
